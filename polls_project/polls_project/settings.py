4c4c197c354058803f3f535df9bb674968e2a22b
rrrrs09 <akzholov.renat@gmail.com>
2020-12-28 17:34:47 +0300

    fix template dir

--- polls_project/polls_project/settings.py
+++ polls_project/polls_project/settings.py
@@ -40,7 +40,9 @@ ROOT_URLCONF = 'polls_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [
+            BASE_DIR + '/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
